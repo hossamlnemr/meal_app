6cb731252c948e3044352b08e67fd64ad3b0d315
Hossam eldeen Hassan <95591772+hossamlnemr@users.noreply.github.com>
2023-12-03 21:54:52 +0200

    create hos.dart

---
+++ lib/hos/hos.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Hos extends StatelessWidget {
+  const Hos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
